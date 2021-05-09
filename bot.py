384f1c88b2fd6d0414ccfed305898aa52bcd8fdb
Luis Marin <luimarpen@alum.us.es>
2021-05-09 21:03:03 +0200

    Cambiando la libreria de icalendar a ics por disponer de mejor manejo y documentación. Definiendo funciones para definir timestamp de principio a final de mes

--- bot.py
+++ bot.py
@@ -1,9 +1,38 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging, telegram
+import ics
+import datetime
+import pytz
+import calendar
+
+
+#Iniciamos el logging en la ventana de consola para mostrar información
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#Este es el token del bot que se ha generado con BotFather.
 tokenbot= None
 with open("token.txt") as f:
     tokenbot = f.read().strip()
 bot=telegram.Bot(token=tokenbot)
+
+#Cargamos el calendario
+ficherocalendario= open('evento con asistentes.txt','r')
+calendario=ics.icalendar.Calendar(ficherocalendario.read())
+ficherocalendario.close()
+
+#Función para calcular el timestamp del primer dia del mes
+def timestampmesinicio():
+    fecha=datetime.date.today()
+    tstamp=datetime.datetime(fecha.year,fecha.month,1,0,0,0,tzinfo=pytz.timezone('Europe/Madrid'))
+    return tstamp
+
+#Función para calcular el timestamp del último día del mes
+def timestampmesfinal():
+    fecha=datetime.date.today()
+    tstamp=datetime.datetime(fecha.year,fecha.month,calendar.monthlen(fecha.year,fecha.month),23,59,59,tzinfo=pytz.timezone('Europe/Madrid'))
+    return tstamp
 
 #Creamos una funcion de eco, que repite el mensaje que recibe
 def echo(update, context):
@@ -17,21 +46,28 @@ def caps(update, context):
 #Esta funcion es la que inicia el bot cuando entra en contacto con un usuario
 def start(update, context):
     #Aqui creamos el teclado por botones que se le mostraraa al usuario en esta funcion
-    kb = [[telegram.KeyboardButton('Boton 1'),telegram.KeyboardButton('Boton 2')],
-          [telegram.KeyboardButton('Boton 3'),telegram.KeyboardButton('Boton 4')],
-          [telegram.KeyboardButton('Boton 5'),telegram.KeyboardButton('Boton 6')]]
-
+    kb = [[telegram.KeyboardButton('/guardiasdisponibles'),telegram.KeyboardButton('/guardiaspropias')],
+          [telegram.KeyboardButton('Boton 3'),telegram.KeyboardButton('Boton 4')]]
     kb_markup = telegram.ReplyKeyboardMarkup(kb,resize_keyboard=True)
     bot.send_message(chat_id=update.message.chat_id,
                      text="your message",
                      reply_markup=kb_markup)
+#Esta funcion representa las guardias disponibles
+def guardiasdisponibles(update, context):
+    for e in calendario.events:
+        print(e.begin)
+        print(e.end)
+    
+    print('')
 
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+print(calendario.events)
+print(datetime.datetime.now())
+print(timestampmesinicio())
+print(timestampmesfinal())
 updater = Updater(token=tokenbot, use_context=True)
 dispatcher = updater.dispatcher
 
-#La función start se le asigna a la funcion start del bot
+#La función start se le asigna a la funcion start del bot. Esta se llama cuando un usuario utiliza el bot por primera vez
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
@@ -45,3 +81,7 @@ updater.start_polling()
 caps_handler = CommandHandler('caps', caps)
 dispatcher.add_handler(caps_handler)
 
+
+#Añadimos función de guardias disponibles
+gdisp_handler=CommandHandler('guardiasdisponibles',guardiasdisponibles)
+dispatcher.add_handler(gdisp_handler)
